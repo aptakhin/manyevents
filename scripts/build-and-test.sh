acc717fa03210411cd51b39fd04b3fb8a365dbcd
Alexander Ptakhin <me@aptakhin.name>
2024-12-29 19:13:05 +0100

    Wtf connection reset

--- scripts/build-and-test.sh
+++ scripts/build-and-test.sh
@@ -6,5 +6,6 @@ make build
 docker compose up -d --build
 ./scripts/wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT -t 30
 ./scripts/wait-for-it.sh $CLICKHOUSE_HOST:$CLICKHOUSE_PORT -t 30
+sleep 2
 make migrate
 make test
